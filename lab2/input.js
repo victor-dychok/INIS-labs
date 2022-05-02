b219a047ec6e0902887555d06ff43a422ea5e89e
victor-dychok <vitiaduchok2015@gmail.com>
2022-05-02 17:32:20 +0300

    Update input.js

--- lab2/input.js
+++ lab2/input.js
@@ -3,6 +3,11 @@ const workSpace = document.querySelector('#workspace');
 let positionX;
 let positionY;
 let escFunction;
+let touchStartX;
+let touchStartY;
+let isDraged = false;
+let object;
+let objectDoubleTouch;
 
 for(let i = 0; i < draggableElements.length; i++)
     {
@@ -17,6 +22,44 @@ for(let i = 0; i < draggableElements.length; i++)
             drop = true;
             });
         
+
+        workSpace.addEventListener('touchstart', (e) => {
+            if (e.targetTouches[1] != undefined)
+            {
+                object.onmousemove = null;
+                object.style.left = touchStartX;
+                object.style.top = touchStartY;
+            }
+            if (isDraged)
+            {
+                object.onmousemove = null;
+                object.ontouchend = null;
+                object.style.left = touchStartX;
+                object.style.top = touchStartY;
+                touchStartX = null;
+                touchStartY = null;
+                isDraged = false;
+            }
+            if (e.target != workSpace)
+            {
+                object = e.target;
+                touchStartX = e.target.style.left;
+                touchStartY = e.target.style.top;
+                object.ontouchmove = (e) => {    
+                    isDraged = true;
+                    draggableElements[i].style.top = e.targetTouches[0].pageY - object.offsetHeight/2 + 'px';
+                    draggableElements[i].style.left = e.targetTouches[0].pageX - object.offsetWidth/2 + 'px';
+                }
+                object.ontouchend = () => {
+                    isDraged = false;
+                    touchStartX = null;
+                    touchStartY = null;
+                    object.ontouchmove = null;
+                }
+            }
+        })
+
+
 
         draggableElements[i].addEventListener('dragend', (e)=>{
             if (drop)
@@ -50,6 +93,9 @@ for(let i = 0; i < draggableElements.length; i++)
         })
         
         draggableElements[i].addEventListener('dblclick', (e)=>{
+            objectDoubleTouch = e.target;
+            touchStartX = objectDoubleTouch.style.left;
+            touchStartY = objectDoubleTouch.style.top;
                 positionX = e.offsetX;
                 positionY = e.offsetY;
             workSpace.onmousemove = (e)=>
@@ -59,41 +105,22 @@ for(let i = 0; i < draggableElements.length; i++)
             }
         })
         
-        draggableElements[i].addEventListener('touchmove', (e)=>
-            {
-                draggableElements[i].style.top = e.targetTouches[0].pageY - draggableElements[i].offsetHeight/2 + 'px';
-                draggableElements[i].style.left = e.targetTouches[0].pageX - draggableElements[i].offsetWidth/2 + 'px';
-            
-        })
-        
-        draggableElements[i].addEventListener('touchstart', (e)=>{
-            if(e.target.style.background == 'blue'){
-                workSpace.ontouchmove=(e)=>
-                {     
-                    draggableElements[i].style.top = e.targetTouches[0].pageY - draggableElements[i].offsetHeight/2 + 'px';
-                    draggableElements[i].style.left = e.targetTouches[0].pageX - draggableElements[i].offsetWidth/2 + 'px';
-                }
+        document.addEventListener('keydown', function(e) {
+            switch (e.keyCode) {
+                case 37:
+                    draggableElements[0].style.left = 0 + 'px';
+                    break;
+                case 38:
+                   alert('up');
+                    break;
+                case 39:
+                    draggableElements[0].style.left = 100 + 'px';
+                    break;
+                case 40:
+                    alert('down');
+                    break;
             }
-        })
+        });
+
         
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
